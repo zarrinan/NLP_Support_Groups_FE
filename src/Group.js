54a2ac09371640070fd07f4175ed6faf33d6a9bd
Zarrina <zarrina_@hotmail.com>
2019-05-02 09:41:04 -0400

    Add links to support groups

--- src/Group.js
+++ src/Group.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 export default function Group (props){
+  let url = `https://www.reddit.com/r/${props.group[0]}/`
+  console.log(url)
+
     return (
-        <li className='group-name'>{props.group[0]}</li>
+        <li key={props.i} className='group-name'>
+           <a className='group-link' href={url}>
+              r/{props.group[0]}
+          </a>
+        </li>
       )
 }
 
